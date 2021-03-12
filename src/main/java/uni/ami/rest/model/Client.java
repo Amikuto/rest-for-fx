ae0ae77515e500c70601d53384b8b84f39a41b79
Damir Kurtaev <56434766+Amikuto@users.noreply.github.com>
2021-03-12 18:58:57 +0300

    1: some code
    rest для javafx

---
+++ src/main/java/uni/ami/rest/model/Client.java
@@ -0,0 +1,70 @@
+package uni.ami.rest.model;
+
+import java.time.LocalDate;
+
+public class Client {
+
+    private Integer id;
+    private String name;
+    private Integer postalCode;
+    private String city;
+    private LocalDate birthday;
+    private String surname;
+    private String street;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Integer getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(Integer postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+}
---
+++ src/test/java/uni/ami/rest/RestApplicationTests.java
@@ -0,0 +1,13 @@
+package uni.ami.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
